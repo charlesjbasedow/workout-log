fb477ad1db0ef6a1c28eb6bc30564f4884d04c6a
charlesjbasedow <cbasedow@outlook.com>
2022-03-06 15:49:05 -0500

    add auth/create

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -14,6 +14,19 @@ function index(req, res) {
   })
 }
 
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+  Workout.create(req.body)
+  .then(workout => {
+    res.redirect('/workouts')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/workouts')
+  })
+}
+
 export {
-  index
+  index,
+  create
 }
--- routes/workouts.js
+++ routes/workouts.js
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import * as workoutsCtrl from '../controllers/workouts.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', workoutsCtrl.index)
+router.post('/', workoutsCtrl.create)
 
 
 

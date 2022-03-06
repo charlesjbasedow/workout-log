b7519367d6e5da2faa3e9931aac44996e3ba122a
charlesjbasedow <cbasedow@outlook.com>
2022-03-06 14:36:14 -0500

    build controller func

--- controllers/workouts.js
+++ controllers/workouts.js
@@ -1,7 +1,17 @@
 import { Workout } from '../models/workout.js'
 
 function index(req, res) {
-  console.log("Workout")
+  Workout.find({})
+  .then(workouts => {
+    res.render('workouts/index', {
+      workouts,
+      title: "Workout Log"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/workouts")
+  })
 }
 
 export {

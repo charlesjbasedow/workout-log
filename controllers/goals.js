5451ace85d3c644e38c833ae6fda577844e29c80
charlesjbasedow <cbasedow@outlook.com>
2022-03-09 00:27:00 -0500

    build goal edit/update

--- controllers/goals.js
+++ controllers/goals.js
@@ -27,7 +27,41 @@ function create(req, res) {
   })
 }
 
+function edit(req, res) {
+  Goal.findById(req.params.id)
+  .then(goal => {
+    res.render("goals/edit", {
+      goal,
+      title: "Edit Info"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/goals")
+  })
+}
+
+function update(req, res) {
+  Goal.findById(req.params.id)
+  .then(goal => {
+    if (goal.owner.equals(req.user.profile._id)) {
+      goal.updateOne(req.body, {new: true})
+      .then(() => {
+        res.redirect(`/goals/${req.params.id}`)
+      })
+    } else {
+      throw new Error("NOT AUTHORIZED")
+    }
+  })
+  .catch(err => {
+    console.log("the error:", err)
+    res.redirect("/goals")
+  })
+}
+
 export {
   index,
-  create
+  create,
+  edit,
+  update
 }
--- routes/goals.js
+++ routes/goals.js
@@ -6,6 +6,8 @@ const router = Router()
 
 router.get('/', isLoggedIn, goalsCtrl.index)
 router.post('/', isLoggedIn, goalsCtrl.create)
+router.get("/:id/edit", isLoggedIn, goalsCtrl.edit)
+router.put("/:id", isLoggedIn, goalsCtrl.update)
 
 
 export {
